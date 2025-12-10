1c1bdc7488ba28de0ea2ae5105496ac71121ce8b
Matica Laurentiu-Andrei <lau.matica84@gmail.com>
2025-12-10 02:01:51 +0200

    Al doilea commit

--- validate_ip.sh
+++ validate_ip.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+check_ip() {
+    local hostname="$1"
+    local ip_din_hosts="$2"
+    local dns_server="$3"
+
+    ip_rezolvat=$(host -t A "$hostname" "$dns_server" 2>/dev/null | grep 'has address' | awk '{print $NF}' | head -n 1)
+
+    if [[ -n "$ip_rezolvat" && "$ip_din_hosts" != "$ip_rezolvat" ]]; then
+        echo "Bogus IP for $hostname in /etc/hosts!"
+    fi
+}
+
 cat /etc/hosts | while read ip hostname garbage;
 do
-        if [[ -n "$ip" && "$ip" =~ ^[0-9] ]]; then
-
-                ip_corect=$(nslookup "$hostname" | grep 'Address:' | head -n -1 | tail -n 1 | awk '{print $2}')
-
-                if [[ -n "$ip_corect" ]]; then
-
-                        if [[ "$ip" != "$ip_corect" ]]; then
-
-                                echo "Bogus IP for $hostname in /etc/hosts!"
-                        fi
-                fi
-        fi
-done  
+    if [[ -n "$ip" && "$ip" =~ ^[0-9] && -n "$hostname" ]]; then
+        check_ip "$hostname" "$ip" "$DEFAULT_DNS"
+    fi
+done
